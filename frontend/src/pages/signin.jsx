003750a6585eccd570b8799e074065dad0b54801
Sanket Mekwan <mecwansanket333@gmail.com>
2024-11-09 00:39:01 +0530

    Small fix (#14)
    * Fix: not able to enter desciption when adding note & auto-filling password css apply
    
    * Fix #4: better validation for Authentication form
    
    * added build

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast';
 import { signup } from '../services/auth';
@@ -27,75 +27,63 @@ function Signin() {
     text: "",
     err: ""
   })
-  let validate_and_set_mail = (val) => {
-    let mailvalue = val;
-    let mailerrr = '';
+  let [errVisible, setErrVisible] = useState(false);
 
+  useEffect(() => {
+    validateBody();
+  }, [name.text, mail.text, password.text, cpassword.text]);
+
+  let validateBody = () => {
+    let namevalue = name.text;
+    let nameerr = '';
+    if (namevalue == '') nameerr = 'name is required!';
+    else if (namevalue.length < 3) nameerr = 'name is too short!';
+    else nameerr = '';
+    setname((pre) => { return { ...pre, err: nameerr } });
+
+    let mailvalue = mail.text;
+    let mailerrr = '';
     if (mailvalue == "")
-      mailerrr = 'required'
-    else if (mailvalue.match(/@/) == null)
-      mailerrr = '@ must be required'
+      mailerrr = 'email is required!'
     else if (/^[a-zA-Z0-9!#%^&*]{3,}@[a-z]{2,12}\.[a-z.]{2,}$/.test(mailvalue) == false)
-      mailerrr = 'invalid mail';
+      mailerrr = 'invalid email!';
     else
       mailerrr = ''
+    setmail((pre) => { return { ...pre, err: mailerrr } });
 
-    setmail((pre) => { return { text: mailvalue, err: mailerrr } });
-    if (mailerrr) return false;
-    return true;
-
-  }
-
-  let validate_and_set_name = (val) => {
-    let err = '';
-    if (val == '') err = 'required';
-    else if (val.length < 3) err = ' atleast 2 char';
-    else err = '';
-    setname({ text: val, err });
-    if (err) return false;
-    return true;
-  }
-
-  let validate_and_set_password = (val) => {
-    let passwordval = val;
+    let passwordval = password.text;
     let passworderr = '';
 
     if (passwordval == '')
-      passworderr = 'required'
+      passworderr = 'password is required!'
     else if (passwordval.length < 8)
-      passworderr = 'atleast 8 charcter'
+      passworderr = 'password too short!'
+    else if (passwordval.length > 15)
+      passworderr = 'password too long!'
+    else if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,15}$/.test(passwordval) == false)
+      passworderr = 'password too weak!'
     else
       passworderr = ''
+    setpassword((pre) => { return { ...pre, err: passworderr } });
 
-    setpassword({ text: passwordval, err: passworderr });
-    if (passworderr) return false;
-    return true;
-
-  }
-
-  let validate_and_set_cpassword = (val) => {
-    let err = '';
-    if (val == '') err = 'required'
-    else if (val != password.text) {
-      err = 'wrong confirm password';
+    let cpasswordval = cpassword.text;
+    let cpassworderr = '';
+    if (cpasswordval == '') cpassworderr = 'confirm password is required!'
+    else if (cpasswordval != password.text) {
+      cpassworderr = 'confirm password not matching!';
     } else {
-      if (password.err) err = password.err;
-      else err = '';
+      if (password.err) cpassworderr = password.err;
+      else cpassworderr = '';
     }
-    setcpassword({ text: val, err });
-    if (err) return false;
-    return true;
+    setcpassword((pre) => { return { ...pre, err: cpassworderr } });
   }
-
 
   let handleSignup = async (e) => {
-    let three = validate_and_set_mail(e.target.mail.value)
-    let four = validate_and_set_name(e.target.name.value)
-    let two = validate_and_set_password(e.target.password.value)
-    let one = validate_and_set_cpassword(e.target.cpassword.value);
-
     e.preventDefault();
-    if (one && two && three && four) {
+
+    if (!errVisible) setErrVisible(true);
+    let isValid = (name.err == '' && mail.err == '' && password.err == '' && cpassword.err == '');
+    if (isValid) {
 
       let formData = {
         name: name.text,
@@ -121,48 +109,48 @@ function Signin() {
     }
   }
 
-  if(authStatus == status.AUTH){
+  if (authStatus == status.AUTH) {
     return (<Navigate to="/" />)
   }
 
   return (
     <div className="formcontainer">
-        <div className="loginform">
-          <h2>Create an account</h2>
-          <form autoComplete='off' id="myform" onSubmit={handleSignup}>
-            <div className="form_item">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" className={name.err && 'err'} value={name.text}
-                onChange={(e) => validate_and_set_name(e.target.value)} />
-              {name.err ? <span className='err_msg'>{name.err}</span> : null}
+      <div className="loginform">
+        <h2>Create an account</h2>
+        <form autoComplete='off' id="myform" onSubmit={handleSignup}>
+          <div className="form_item">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" className={errVisible && name.err && 'err'} value={name.text}
+              onChange={(e) => setname((pre) => { return { ...pre, text: e.target.value } })} />
+            {errVisible && name.err ? <span className='err_msg'>{name.err}</span> : null}
 
-            </div>
-            <div className="form_item">
-              <label htmlFor="mail">Email</label>
-              <input type="email" name="mail" id="mail" className={mail.err && 'err'} value={mail.text}
-                onChange={(e) => validate_and_set_mail(e.target.value)} />
-              {mail.err ? <span className='err_msg'>{mail.err}</span> : null}
-            </div>
-            <div className="form_item">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" className={password.err && 'err'} value={password.text} onChange={(e) => validate_and_set_password(e.target.value)} />
-              {password.err ? <span className='err_msg'>{password.err}</span> : null}
-
-            </div>
-            <div className="form_item">
-              <label htmlFor="cpassword">confirm Password</label>
-              <input type="password" name="cpassword" id="cpassword" className={cpassword.err && 'err'} value={cpassword.text} onChange={(e) => validate_and_set_cpassword(e.target.value)} />
-              {cpassword.err ? <span className='err_msg'>{cpassword.err}</span> : null}
-
-            </div>
-            <input type="submit" value="Signup" />
-
-          </form>
-          <div className="link">
-            Have an account?
-            <Link to="/login"> Login</Link>
           </div>
+          <div className="form_item">
+            <label htmlFor="mail">Email</label>
+            <input type="email" name="mail" id="mail" className={errVisible && mail.err && 'err'} value={mail.text}
+              onChange={(e) => setmail((pre) => { return { ...pre, text: e.target.value } })} />
+            {errVisible && mail.err ? <span className='err_msg'>{mail.err}</span> : null}
+          </div>
+          <div className="form_item">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" className={errVisible && password.err && 'err'} value={password.text} onChange={(e) => setpassword((pre) => { return { ...pre, text: e.target.value } })} />
+            {errVisible && password.err ? <span className='err_msg'>{password.err}</span> : null}
+
+          </div>
+          <div className="form_item">
+            <label htmlFor="cpassword">confirm Password</label>
+            <input type="password" name="cpassword" id="cpassword" className={errVisible && cpassword.err && 'err'} value={cpassword.text} onChange={(e) => setcpassword((pre) => { return { ...pre, text: e.target.value } })} />
+            {errVisible && cpassword.err ? <span className='err_msg'>{cpassword.err}</span> : null}
+
+          </div>
+          <input type="submit" value="Signup" />
+
+        </form>
+        <div className="link">
+          Have an account?
+          <Link to="/login"> Login</Link>
         </div>
+      </div>
     </div>
   )
 }
