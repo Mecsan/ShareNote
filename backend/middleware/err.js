d62927697677da50e4ad88c009e89a49123e0b6f
Mecsan <mecwansanket333@gmail.com>
2024-12-15 10:59:41 +0530

    handled mongoose validation errors, which was returning 500 statuCode

--- backend/middleware/err.js
+++ backend/middleware/err.js
@@ -1,5 +1,16 @@
 module.exports = (err, req, res, next) => {
     let status = res.statusCode == 200 ? 500 : res.statusCode;
     console.log(err);
-    res.status(status).json({ err: err.message })
+
+    // handling mongoose errors
+    if(err.name == 'CastError'){
+        err.message = 'invalid id : ' + err.value;
+        status = 400;
+    }else if(err.name == 'ValidationError'){
+        let msg = Object.values(err.errors).map(val => val.message);
+        err.message = msg.join(', ');
+        status = 400;
+    }
+    
+    res.status(status).json({ err: err.message || 'Something went wrong' })
 }
